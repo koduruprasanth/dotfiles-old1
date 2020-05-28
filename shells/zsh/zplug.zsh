43382758674c68e6fafbb552da55297cc8b3d830
Prasanth Koduru <koduruprasanth@gmail.com>
2020-05-27 23:39:39 -0500

    Setup dotbot

--- shells/zsh/zplug.zsh
+++ shells/zsh/zplug.zsh
@@ -31,10 +31,3 @@ if ! zplug check --verbose; then
 fi
 
 zplug load --verbose
-
-export EDITOR=code
-alias reload='source ~/.zshrc'
-
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="/Users/prasanthk/.sdkman"
-[[ -s "/Users/prasanthk/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/prasanthk/.sdkman/bin/sdkman-init.sh"
